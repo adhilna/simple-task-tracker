5ee420fb40944511b9092c27a16031f6d4a81067
adhilna <adhilchy11@gmail.com>
2026-01-21 17:57:55 +0530

    Finalize Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('TODO', 'Todo'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('DONE', 'Done'),
+    ]
+
+    title = models.CharField(max_length=128, blank=False, null=False)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="TODO"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} in {self.status}"
